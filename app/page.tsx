a941eb5be018e63acac3e7de5b93f4e6200107fc
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-08-23 19:21:09 +0530

    update : navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Speaker from "@/components/Speaker";
 import Schedule from "@/components/Schedule";
 import Image from "next/image";
 import landingBanner from "@/public/about-banner-2.jpg";
-
+import Marquee from "react-fast-marquee";
 import speaker1 from "@/public/speaker.jpg";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
@@ -10,15 +10,23 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#f1f1f1] text-black transition-all overflow-x-hidden">
       <section className="flex flex-col min-h-screen justify-end items-center">
+        <Marquee>
+          <div className="text-lg p-10">
+          BEST PAPER AWARD FOR ALL TRACKS HAVE BEEN DECLARED <span className="hover:cursor-pointer text-blue-300">KNOW MORE</span> 
+          </div>
+        </Marquee>
         <nav
           id="nav"
-          className="flex text-2xl w-full justify-center text-center pt-[150px] "
+          className="flex text-2xl w-full justify-center text-center pt-[50px] "
         >
-          <div className="w-[30%] flex justify-between">
+          <div className="md:flex hidden w-[30%] justify-between">
             <div>Home</div>
-            <div>Smth</div>
-            <div>Smth</div>
-            <div>Smth</div>
+            <div>Schedule</div>
+            <div>Committees</div>
+            <div>Events</div>
+          </div>
+          <div className="md:hidden flex w-[%] justify-between">
+
           </div>
         </nav>
 
@@ -39,7 +47,10 @@ export default function Home() {
       </section>
 
       <section className="flex flex-col justify-center items-center min-h-screen">
-        <div className="text-[70px] font-medium text-center">About</div>
+        <div className="w-[90%] flex justify-start text-[40px]">
+Welcome to ICSCC 2024 <br />
+Thiruvanathapuram, India | August 17 – 19, 2024
+</div>
         <div className="flex w-[90%] mx-auto mt-5 justify-between ">
           <h2 className="max-w-3xl text-xl text-justify">
             We would like to invite you to contribute to and participate in the
@@ -82,20 +93,20 @@ export default function Home() {
 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
 
-      <section id="schedules">
-        <div className="text-[50px] text-center">
+      <section id="schedules" className="md:h-full ">
+        <div className="text-[50px] text-center my-10">
           Dates To Remember
         </div>
-        <div className="flex w-[95%] mx-auto">
+        <div className="flex md:flex-row flex-col w-[95%] mx-auto">
           <Schedule />
           <Schedule />
         </div>
 
-        <div className="flex">
+        <div className="flex md:flex-row flex-col">
           <Schedule />
           <Schedule />
         </div>
-        <div className="flex w-[95%] mx-auto">
+        <div className="flex md:flex-row flex-col  w-[95%] mx-auto">
           <Schedule />
           <Schedule />
         </div>
@@ -103,7 +114,7 @@ export default function Home() {
 
       <section id="speakers" className="mt-5 flex w-[90%] h-full mx-auto">
         <div>
-          <div className="text-[50px]  text-center">Speakers</div>
+          <div className="text-[50px]  text-center my-10">Speakers</div>
           <div className="flex flex-wrap gap-y-2 justify-center ">
             <Speaker
               image={speaker1}
